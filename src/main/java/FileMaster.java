1e7431d84bbae5a77317a57615c6b81731f78042
sfaxi19 <sfaxi19@gmail.com>
2018-01-02 23:12:41 +0300

    first commit

---
+++ src/main/java/FileMaster.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.HashMap;
+
+/**
+ * Created by sfaxi19 on 12.10.16.
+ */
+public class FileMaster {
+
+    public static String loadTextFromFile(String filepath) throws IOException {
+        File f = new File(filepath);
+        BufferedReader fin = new BufferedReader(new FileReader(f));
+        StringBuffer text = new StringBuffer();
+        String line;
+        while ((line = fin.readLine()) != null) {
+            text.append(line);
+        }
+        fin.close();
+        return text.toString();
+    }
+
+    public static void saveTextToFile(String filepath, String text) throws IOException {
+        File f = new File(filepath);
+        BufferedWriter fout = new BufferedWriter(new FileWriter(f));
+        fout.write(text);
+        fout.flush();
+        fout.close();
+        System.out.println("save - " + text.length());
+    }
+
+    public static byte[] loadBytesFromFile(String filepath) throws IOException {
+        File f = new File(filepath);
+        BufferedReader fin = new BufferedReader(new FileReader(f));
+        StringBuffer text = new StringBuffer();
+        String line;
+        // fin.rea
+        while ((line = fin.readLine()) != null) {
+            text.append(line);
+        }
+        fin.close();
+        return text.toString().getBytes();
+    }
+
+}
